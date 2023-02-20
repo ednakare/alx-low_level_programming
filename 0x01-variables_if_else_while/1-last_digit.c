78d845cc17c6339f478dcf4f150cb5052d854f0b
ednakare <ednamuhia02@gmail.com>
2023-02-20 12:39:37 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that assign a random number to variables.
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int ld;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
+	if (ld > 5)
+	{
+		printf("last digit of %d is %d and is greater than 5", n, ld);
+	}
+	{
+	if (ld == 0)
+		printf("last digit of %d is %d and is 0", n, ld);
+	}
+	if (ld < 6 && ld != 0)
+	{
+		printf("last digit of %d is %d and less than 6 and not 0", n, ld);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - A program that that prints the alphabet in lowercase
+ * Return: always 0
+ */
+
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
