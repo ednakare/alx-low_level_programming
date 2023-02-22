09c4186cd6ce83b14cecd96404b118eb1195635b
ednakare <ednamuhia02@gnail.com>
2023-02-22 14:22:11 -0800

    user.name

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  * Return: Always 0
  */
 void print_alphabet(void)
-{
-	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+{
+	char c;
+
+	for (c = 'a'; c  <= 'z'; c++)
 	{
-	_putchar(letter);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
