0adc636c792f12acb9243431099400d2983fdc67
ednakare <ednamuhia02@gnail.com>
2023-02-23 05:49:26 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,22 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if (i % 3 == 0 && i % 5 != 0)   
-	}
+	if (i % 3 == 0 && i % 5 != 0)
+	{
 	printf(" Fizz");
-	{
+	}
 	else if (i % 5 == 0 && i % 3 != 0)
-	}
+	{
 	printf(" Buzz");
-	{
+	}
 	else if (i % 3 == 0 && i % 5 == 0)
-	}
+	{
 	printf(" FizzBuzz");
-	{
-	else if (i == 1)
 	}
-	printf("%d", i);
+	else if (i == 1)
 	{
+	printf("%d", i);
+	}
 	else
 	{
 	printf(" %d", i);
@@ -38,4 +38,5 @@ int main(void)
 
 	return (0);
 }
+
 
